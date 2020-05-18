144ab00aecd8e0426cd9ba13572fa23a4c13b80b
yueranyu <librayuyueran@gmail.com>
2020-05-17 21:20:21 -0500

    input event

--- JS_jQuery_13/js/input-type.js
+++ JS_jQuery_13/js/input-type.js
@@ -1,3 +1,17 @@
 (function() {
+    let pasword = document.getElementById("pwd");
+    let check = document.getElementById("showPwd");
 
+    addEvent(check, 'change', function(e) {
+        let target = e.target || e.srcElement;
+        try {
+            if (target.checked) {
+                pasword.type = 'text';
+            } else {
+                pasword.type = 'password';
+            }
+        } catch (error) {
+            alert('This brower cannot switch type.');
+        }
+    });
 })();
