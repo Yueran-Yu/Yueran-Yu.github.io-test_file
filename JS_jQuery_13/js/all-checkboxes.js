90251e39a205ce1b07924ebe38823230dcf15973
yueranyu <librayuyueran@gmail.com>
2020-05-18 16:05:59 -0500

    OMG git add .! finally I figured this problem, after study other's logical thought, I combined them to my code

---
+++ JS_jQuery_13/js/all-checkboxes.js
@@ -0,0 +1,32 @@
+(function() {
+    let form = document.getElementById('interests');
+    let all = document.getElementById('all');
+    let elements = form.elements;
+    let options = elements.genre;
+    let count = 0;
+
+    addEvent(all, 'change', function(e) {
+        let target = e.target || e.srcElement;
+        for (let i = 0; i < options.length; i++) {
+            options[i].checked = target.checked;
+        }
+        count = target.checked ? options.length : 0;
+        // alert(count);
+    });
+
+    for (let i = 0; i < options.length; i++) {
+        addEvent(options[i], 'change', function(e) {
+            let target = e.target || e.srcElement;
+            if (!target.checked) {
+                all.checked = false;
+                count--;
+                // alert(count);
+            } else {
+                count++;
+                // alert(count);
+            }
+            if (count === options.length)
+                all.checked = true;
+        });
+    }
+})();
